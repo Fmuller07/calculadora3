9aa486bff84d70bc35efe1776861a349898fa146
Fmuller07 <ventaniamuller@gmail.com>
2025-02-12 14:10:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function somarValores(){
+    var num1 = document.getElementById("text1").value;
+    var num2 = document.getElementById("text2").value;
+    var num3 = num1*num2;
+    alert(num3);
+ }
